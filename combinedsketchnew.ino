31dddc05ca7d271e0b25b63442dfb38f38f58d99
Yuanqiao Lin <30318863+lindaqmm97@users.noreply.github.com>
2018-10-03 12:18:52 -0400

    Update combinedsketchnew.ino

--- combinedsketchnew.ino
+++ combinedsketchnew.ino
@@ -1,110 +1,34 @@
-#include <Servo.h>
-#include <SPI.h>
-#include <Adafruit_LSM9DS1.h>
-#include <Adafruit_Sensor.h>
-#include <Adafruit_GPS.h>
-#define motorPin 8 // PWM for motor
-#define pingTrigPin 23 // ping sensor trigger pin (output from Arduino)
-#define pingEchoPin 25 // ping sensor echo pin (input to Arduino)
-#define pingGrndPin 27 // ping sensor ground pin (use digital pin as ground)
-
-#define LSM9DS1_SCK 52  //BDK-mega
-#define LSM9DS1_MISO 50 //BDK-mega
-#define LSM9DS1_MOSI 51 //BDK-mega
-#define LSM9DS1_XGCS 49 //BDK-mega
-#define LSM9DS1_MCS 47 //BDK-mega
-#define servoPin 7 // pin for servo signal
-
-Adafruit_LSM9DS1 lsm = Adafruit_LSM9DS1(LSM9DS1_XGCS, LSM9DS1_MCS);
-HardwareSerial mySerial = Serial1;
-Adafruit_GPS GPS(&Serial1);
-
-Servo steeringServo;
-byte servoAngleDeg = 90;
-byte motorPWM=175;
-byte motorPWMCurrent;
-float pingDistanceCM = 0.0;
-//byte gyroZBias=0;
-//byte servoBias=0;
-float timeRead=20;
-float k_heading =1.5;
-//float GPS_sub=0;
 float gpsPsi;
 float servoAngle=90;
-//float GPSangle;
+int tau=4;
 
-void setup() {
-  // put your setup code here, to run once:
-    Serial.begin(115200);
-    OCR0A = 0xAF;
-    TIMSK0 |= _BV(OCIE0A);
-    GPS.begin(9600);
-    GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCONLY); // minimum information only
-    GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);   // 1 Hz update rate
-    pinMode(motorPin,OUTPUT);
-    analogWrite(motorPin,0);
-    pinMode(pingGrndPin,OUTPUT); digitalWrite(pingGrndPin,LOW);
-    pinMode(pingTrigPin,OUTPUT);
-    pinMode(pingEchoPin,INPUT);
-    lsm.setupAccel(lsm.LSM9DS1_ACCELRANGE_2G);
-    lsm.setupMag(lsm.LSM9DS1_MAGGAIN_4GAUSS);
-    lsm.setupGyro(lsm.LSM9DS1_GYROSCALE_245DPS);
-    steeringServo.attach(servoPin);
-    steeringServo.write(servoAngleDeg);
-//  read bias by taking the average of 10 gyro.z readings  
-    lsm.begin();
-
+double desiredPsi=10;
+if (GPS.parse(GPS.lastNMEA())){
+    if(GPS.speed>0.5){
+       gpsPsi = GPS.angle;}
+    }
+sensors_event_t a, m, g, temp;
+lsm.getEvent(&a, &m, &g, &temp);
+static float heading_est_degrees = 0;
+float partial_sub=gpsPsi-heading_est_degrees;
+Serial.println(partial_sub);
+if(partial_sub<-180){
+   partial_sub+=360;
 }
-
-SIGNAL(TIMER0_COMPA_vect) {
-   char c = GPS.read();
+else if(partial_sub>180){
+   partial_sub-=360;
 }
+heading_est_degrees+=(.001*timeRead)/tau*(gpsPsi-heading_est_degrees)+(.001*timeRead)*g.gyro.z;
+float angle_sub=desiredPsi-heading_est_degrees;
+if(desiredPsi-heading_est_degrees<-180){
+  angle_sub=360+angle_sub;
+}
+else if(desiredPsi-heading_est_degrees>180){
+  angle_sub=angle_sub-360;
+}  
+servoAngle = servoAngleDeg-k_heading * angle_sub; 
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  int tau=4;
-  static unsigned long timer = millis();
-  double desiredPsi=10;
-  motorPWMCurrent=motorPWM;
-  getPingDistanceCM();
-  if(pingDistanceCM<=30.0){
-    motorPWMCurrent=0;
-  } 
-  analogWrite(motorPin,motorPWMCurrent);
-  lsm.read();  /* ask it to read in the data */
-  if (GPS.parse(GPS.lastNMEA())){
-//    if(GPS.speed>0.5){
-      gpsPsi = GPS.angle;}
-//  }
-//  Serial.println(gpsPsi);
-
-  sensors_event_t a, m, g, temp;
-  lsm.getEvent(&a, &m, &g, &temp);
-  static float heading_est_degrees = 0;
-  float partial_sub=gpsPsi-heading_est_degrees;
-//  Serial.println(partial_sub);
-//  if(partial_sub<-180){
-//    partial_sub+=360;
-//  }
-//  else if(partial_sub>180){
-//    partial_sub-=360;
-//  }
-  heading_est_degrees+=(.001*timeRead)/tau*(gpsPsi-heading_est_degrees)+(.001*timeRead)*g.gyro.z;
-//  Serial.println(heading_est_degrees);
-  float angle_sub=desiredPsi-heading_est_degrees;
-//  Serial.println(angle_sub);
-  if(desiredPsi-heading_est_degrees<-180){
-    angle_sub=360+angle_sub;
-  }
-  else if(desiredPsi-heading_est_degrees>180){
-    angle_sub=angle_sub-360;
-  }  
-  servoAngle = servoAngleDeg-k_heading * angle_sub; 
-  
-  Serial.println(angle_sub);
-  Serial.println(GPS.angle);
-  Serial.println("---");  
-  steeringServo.write(constrain(servoAngle,servoAngleDeg-30, servoAngleDeg+30)); 
+steeringServo.write(constrain(servoAngle,servoAngleDeg-30, servoAngleDeg+30)); 
   while(millis()-timer<20){
   }
   timer=millis();
